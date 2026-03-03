9dac192177ea7293b1875dced8e09f640d3fa779
Marty Kube <marty@beavercreekconsulting.com>
2026-03-02 20:35:44 -0500

    Temperature sensors working

--- src/main.cpp
+++ src/main.cpp
@@ -17,122 +17,89 @@
 #include "sensesp/signalk/signalk_output.h"
 #include "sensesp/system/lambda_consumer.h"
 #include "sensesp_app_builder.h"
+#include "sensesp/transforms/linear.h"
+#include "sensesp/ui/config_item.h"
+#include "sensesp_onewire/onewire_temperature.h"
 
+using namespace reactesp;
 using namespace sensesp;
+using namespace sensesp::onewire;
+
 
 // The setup function performs one-time application initialization.
 void setup() {
+  
   SetupLogging(ESP_LOG_DEBUG);
 
   // Construct the global SensESPApp() object
   SensESPAppBuilder builder;
   sensesp_app = (&builder)
                     // Set a custom hostname for the app.
-                    ->set_hostname("my-sensesp-project")
+                    ->set_hostname("sensoresp")
                     // Optionally, hard-code the WiFi and Signal K server
                     // settings. This is normally not needed.
-                    //->set_wifi_client("My WiFi SSID", "my_wifi_password")
+                    // ->set_wifi_client("signalkpi", "signalkpi")
                     //->set_wifi_access_point("My AP SSID", "my_ap_password")
-                    //->set_sk_server("192.168.10.3", 80)
+                    // ->set_sk_server("192.168.4.0", 80)
                     ->get_app();
 
-  // GPIO number to use for the analog input
-  const uint8_t kAnalogInputPin = 36;
-  // Define how often (in milliseconds) new samples are acquired
-  const unsigned int kAnalogInputReadInterval = 500;
-  // Define the produced value at the maximum input voltage (3.3V).
-  // A value of 3.3 gives output equal to the input voltage.
-  const float kAnalogInputScale = 3.3;
 
-  // Create a new Analog Input Sensor that reads an analog input pin
-  // periodically.
-  auto analog_input = std::make_shared<AnalogInput>(
-      kAnalogInputPin, kAnalogInputReadInterval, "", kAnalogInputScale);
+  uint8_t pin = 4;
+  DallasTemperatureSensors* dts = new DallasTemperatureSensors(pin);
+  // Define how often SensESP should read the sensor(s) in milliseconds
+  uint read_delay = 500;
 
-  // Add an observer that prints out the current value of the analog input
-  // every time it changes.
-  analog_input->attach([analog_input]() {
-    debugD("Analog input value: %f", analog_input->get());
-  });
+  // Measure exhaust temperature
+  auto* exhaust_temp =
+      new OneWireTemperature(dts, read_delay, "/exhaustTemperature/oneWire");
+  ConfigItem(exhaust_temp)
+    ->set_title("Exhaust Temperature")
+    ->set_description("Temperature of the engine exhaust")
+    ->set_sort_order(100);
 
-  // Set GPIO pin 15 to output and toggle it every 650 ms
+  auto* exhaust_temp_calibration =
+      new Linear(1.0, 0.0, "/exhaustTemperature/linear");
+  ConfigItem(exhaust_temp_calibration)
+    ->set_title("Exhaust Temperature Calibration")
+    ->set_description("Calibration for the exhaust temperature sensor")
+    ->set_sort_order(200);
 
-  const uint8_t kDigitalOutputPin = 15;
-  const unsigned int kDigitalOutputInterval = 650;
-  pinMode(kDigitalOutputPin, OUTPUT);
-  event_loop()->onRepeat(kDigitalOutputInterval, [kDigitalOutputPin]() {
-    digitalWrite(kDigitalOutputPin, !digitalRead(kDigitalOutputPin));
-  });
+  auto* exhaust_temp_sk_output = new SKOutputFloat(
+      "propulsion.mainEngine.exhaustTemperature", "/exhaustTemperature/skPath");
 
-  // Read GPIO 14 every time it changes
+  ConfigItem(exhaust_temp_sk_output)
+      ->set_title("Exhaust Temperature Signal K Path")
+      ->set_description("Signal K path for the exhaust temperature")
+      ->set_sort_order(300);
 
-  const uint8_t kDigitalInput1Pin = 14;
-  auto digital_input1 = std::make_shared<DigitalInputChange>(
-      kDigitalInput1Pin, INPUT_PULLUP, CHANGE);
+  exhaust_temp->connect_to(exhaust_temp_calibration)
+  ->connect_to(exhaust_temp_sk_output);
 
-  // Connect the digital input to a lambda consumer that prints out the
-  // value every time it changes.
-
-  // Test this yourself by connecting pin 15 to pin 14 with a jumper wire and
-  // see if the value changes!
-
-  auto digital_input1_consumer = std::make_shared<LambdaConsumer<bool>>(
-      [](bool input) { debugD("Digital input value changed: %d", input); });
-
-  digital_input1->connect_to(digital_input1_consumer);
-
-  // Create another digital input, this time with RepeatSensor. This approach
-  // can be used to connect external sensor library to SensESP!
-
-  const uint8_t kDigitalInput2Pin = 13;
-  const unsigned int kDigitalInput2Interval = 1000;
-
-  // Configure the pin. Replace this with your custom library initialization
-  // code!
-  pinMode(kDigitalInput2Pin, INPUT_PULLUP);
-
-  // Define a new RepeatSensor that reads the pin every 100 ms.
-  // Replace the lambda function internals with the input routine of your custom
-  // library.
-
-  // Again, test this yourself by connecting pin 15 to pin 13 with a jumper
-  // wire and see if the value changes!
-
-  auto digital_input2 = std::make_shared<RepeatSensor<bool>>(
-      kDigitalInput2Interval,
-      [kDigitalInput2Pin]() { return digitalRead(kDigitalInput2Pin); });
-
-  // Connect the analog input to Signal K output. This will publish the
-  // analog input value to the Signal K server every time it changes.
-  auto aiv_metadata = std::make_shared<SKMetadata>("V", "Analog input voltage");
-  auto aiv_sk_output = std::make_shared<SKOutput<float>>(
-      "sensors.analog_input.voltage",   // Signal K path
-      "/Sensors/Analog Input/Voltage",  // configuration path, used in the
-                                        // web UI and for storing the
-                                        // configuration
-      aiv_metadata
-  );
-
-  ConfigItem(aiv_sk_output)
-      ->set_title("Analog Input Voltage SK Output Path")
-      ->set_description("The SK path to publish the analog input voltage")
+  // Measure coolant temperature
+  auto coolant_temp =
+      new OneWireTemperature(dts, read_delay, "/coolantTemperature/oneWire");
+  ConfigItem(coolant_temp)
+      ->set_title("Coolant Temperature")
+      ->set_description("Temperature of the engine coolant")
       ->set_sort_order(100);
 
-  analog_input->connect_to(aiv_sk_output);
-
-  // Connect digital input 2 to Signal K output.
-  auto di2_metadata = std::make_shared<SKMetadata>("", "Digital input 2 value");
-  auto di2_sk_output = std::make_shared<SKOutput<bool>>(
-      "sensors.digital_input2.value",    // Signal K path
-      "/Sensors/Digital Input 2/Value",  // configuration path
-      di2_metadata
-  );
-
-  ConfigItem(di2_sk_output)
-      ->set_title("Digital Input 2 SK Output Path")
+  auto coolant_temp_calibration =
+      new Linear(1.0, 0.0, "/coolantTemperature/linear");
+  ConfigItem(coolant_temp_calibration)
+      ->set_title("Coolant Temperature Calibration")
+      ->set_description("Calibration for the coolant temperature sensor")
       ->set_sort_order(200);
 
-  digital_input2->connect_to(di2_sk_output);
+  auto coolant_temp_sk_output = new SKOutputFloat(
+      "propulsion.mainEngine.coolantTemperature", "/coolantTemperature/skPath");
+
+  ConfigItem(coolant_temp_sk_output)
+      ->set_title("Coolant Temperature Signal K Path")
+      ->set_description("Signal K path for the coolant temperature")
+      ->set_sort_order(300);
+
+  coolant_temp->connect_to(coolant_temp_calibration)
+      ->connect_to(coolant_temp_sk_output);
 
   // To avoid garbage collecting all shared pointers created in setup(),
   // loop from here.
